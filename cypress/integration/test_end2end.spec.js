906675b204ca3aa7544f5dfeaf66ea92dd08ee73
tgskyline <tigomes.santos@gmail.com>
2021-07-22 20:01:49 -0300

    Insert Account

--- cypress/integration/test_end2end.spec.js
+++ cypress/integration/test_end2end.spec.js
@@ -1,14 +1,28 @@
 ///<reference types="cypress" />
 
+// Login
 describe('Test End 2 End', () => {
-
-  it('login', function () {
+  before(() => {
     cy.visit('https://barrigareact.wcaquino.me/')
-    cy.fixture('userData').as('data').then(() => {
+    cy.fixture('userData').as('data').then(function () {
       cy.get('[data-test=email]').type(this.data.login)
       cy.get('[data-test=passwd]').type(this.data.senha)
       cy.get('.btn').click()
-      cy.get('.toast-message').should('contain', 'Bem vindo')
+      cy.get('.toast-message')
+        .should('contain', 'Bem vindo')
+        .click()
     })
   })
+  // Inserir conta
+  it('Insert Account', () => {
+    cy.get('[data-test=menu-settings]').click()
+    cy.get('[href="/contas"]').click()
+    cy.get('.form-control').type('Financiamento')
+    cy.get('.btn').click()
+    cy.get('.toast-message').should('exist')
+    cy.get('.toast-close-button').click()
+
+  })
+
 })
+
