3cb5bc8bb2d64c394f5fbbd19390d2caadab31c8
tgskyline <tigomes.santos@gmail.com>
2021-09-09 22:04:28 -0300

    Insert Movimenting

--- cypress/support/locators.js
+++ cypress/support/locators.js
@@ -7,12 +7,21 @@ const locators = {
   MENU: {
     SETTINGS: '[data-test=menu-settings]',
     CONTAS: '[href="/contas"]',
-    RESET: '[href="/reset"]'
+    RESET: '[href="/reset"]',
+    MOVIMENTACAO: '[data-test=menu-movimentacao]'
+
   },
   CONTAS: {
     NOME: '[data-test=nome]',
     BTN_SALVAR: '.btn',
     XP_BTN_ALTERAR: "//table//td[contains(.,'Financiamento')]/..//i[@class='far fa-edit']"
+  },
+
+  MOVIMENTACAO: {
+    DESCRICAO: '[data-test=descricao]',
+    VALOR: '[data-test=valor]',
+    INTERESSADO: '[data-test=envolvido]',
+    BTN_SALVAR: '.btn-primary'
   },
   MESSAGE: '.toast-message'
 
